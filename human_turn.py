34483fbfe3a85354a097c8a7b57438062d360254
balast <Adam.Lewis@utexas.edu>
2017-05-05 18:15:05 -0500

    Bug Fixed

--- human_turn.py
+++ human_turn.py
@@ -1,17 +1,10 @@
 import random
-from adam_best_alg import adam_best_alg
+# from adam_best_alg import adam_best_alg
 from ai_turn import ai_turn
 
 def human_turn(partial_gs):
     # # first available card
     # return partial_gs['playable_cards'][0]
 
-    # play a random card
-    card_index = random.randint(0, len(partial_gs['playable_cards'])-1)
-    return partial_gs['playable_cards'][card_index]
-
-    # # use the same strategy as the ai
-    # return ai_turn(partial_gs)
-
-    # # use my best algorithm
-    # return adam_best_alg(partial_gs)
+    # use the same strategy as the ai
+    return ai_turn(partial_gs)
